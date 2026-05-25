218be431d68e6008b2ef93458837f2d9785574a7
iaminnovator-07 <swadeepbansode2007@gmail.com>
2026-05-25 20:44:35 +0530

    Update motion_detection_security_alarm.ino

--- 2020-early-experimentation/motion-detection-security-alarm/src/motion_detection_security_alarm.ino
+++ 2020-early-experimentation/motion-detection-security-alarm/src/motion_detection_security_alarm.ino
@@ -1,1 +1,37 @@
-// Placeholder
+int pirSensor = 2;
+
+int buzzer = 8;
+int led = 9;
+
+void setup() {
+
+  pinMode(pirSensor, INPUT);
+
+  pinMode(buzzer, OUTPUT);
+  pinMode(led, OUTPUT);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  int motion = digitalRead(pirSensor);
+
+  Serial.println(motion);
+
+  if (motion == HIGH) {
+
+    digitalWrite(buzzer, HIGH);
+    digitalWrite(led, HIGH);
+
+  } else {
+
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, LOW);
+
+  }
+
+  delay(100);
+
+}
