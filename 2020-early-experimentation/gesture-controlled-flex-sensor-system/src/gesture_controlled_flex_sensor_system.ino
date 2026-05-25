dc8f7c7f3d0c475ec7624cf9259c580de561f455
iaminnovator-07 <swadeepbansode2007@gmail.com>
2026-05-25 20:31:07 +0530

    Update gesture_controlled_flex_sensor_system.ino

--- 2020-early-experimentation/gesture-controlled-flex-sensor-system/src/gesture_controlled_flex_sensor_system.ino
+++ 2020-early-experimentation/gesture-controlled-flex-sensor-system/src/gesture_controlled_flex_sensor_system.ino
@@ -1,1 +1,35 @@
-// Placeholder
+int flexSensor = A0;
+
+int led1 = 8;
+int led2 = 9;
+
+void setup() {
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  int flexValue = analogRead(flexSensor);
+
+  Serial.println(flexValue);
+
+  if (flexValue > 700) {
+
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+
+  } else {
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+
+  }
+
+  delay(100);
+
+}
