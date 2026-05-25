e4d59cf04580f0a290287c7a53889815754c5950
iaminnovator-07 <swadeepbansode2007@gmail.com>
2026-05-25 20:48:57 +0530

    Update soil_moisture_alert.ino

--- 2020-early-experimentation/soil-moisture-alert/src/soil_moisture_alert.ino
+++ 2020-early-experimentation/soil-moisture-alert/src/soil_moisture_alert.ino
@@ -1,1 +1,35 @@
-// Placeholder
+int moistureSensor = A0;
+
+int buzzer = 8;
+int led = 9;
+
+void setup() {
+
+  pinMode(buzzer, OUTPUT);
+  pinMode(led, OUTPUT);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  int moistureValue = analogRead(moistureSensor);
+
+  Serial.println(moistureValue);
+
+  if (moistureValue > 700) {
+
+    digitalWrite(buzzer, HIGH);
+    digitalWrite(led, HIGH);
+
+  } else {
+
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, LOW);
+
+  }
+
+  delay(100);
+
+}
