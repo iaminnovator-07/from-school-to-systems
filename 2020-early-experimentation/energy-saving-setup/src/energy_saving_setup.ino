abcfdec7e7e8c00827098173de849fcdeabf05f2
iaminnovator-07 <swadeepbansode2007@gmail.com>
2026-05-25 20:25:28 +0530

    Update energy_saving_setup.ino

--- 2020-early-experimentation/energy-saving-setup/src/energy_saving_setup.ino
+++ 2020-early-experimentation/energy-saving-setup/src/energy_saving_setup.ino
@@ -1,1 +1,35 @@
-// Placeholder
+int ldr = A0;
+
+int relay = 8;
+int led = 9;
+
+void setup() {
+
+  pinMode(relay, OUTPUT);
+  pinMode(led, OUTPUT);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  int lightValue = analogRead(ldr);
+
+  Serial.println(lightValue);
+
+  if (lightValue < 500) {
+
+    digitalWrite(relay, HIGH);
+    digitalWrite(led, HIGH);
+
+  } else {
+
+    digitalWrite(relay, LOW);
+    digitalWrite(led, LOW);
+
+  }
+
+  delay(100);
+
+}
