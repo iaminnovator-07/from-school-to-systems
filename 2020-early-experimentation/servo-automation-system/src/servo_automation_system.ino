bf15babf04937166b64fa77f0c77d1bd4dc7492d
iaminnovator-07 <swadeepbansode2007@gmail.com>
2026-05-25 20:46:38 +0530

    Update servo_automation_system.ino

--- 2020-early-experimentation/servo-automation-system/src/servo_automation_system.ino
+++ 2020-early-experimentation/servo-automation-system/src/servo_automation_system.ino
@@ -1,1 +1,30 @@
-// Placeholder
+#include <Servo.h>
+
+Servo myServo;
+
+int potPin = A0;
+int potValue;
+int angle;
+
+void setup() {
+
+  myServo.attach(9);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  potValue = analogRead(potPin);
+
+  angle = map(potValue, 0, 1023, 0, 180);
+
+  myServo.write(angle);
+
+  Serial.print("Servo Angle: ");
+  Serial.println(angle);
+
+  delay(15);
+
+}
