620921a8908f5414bf70ecbfc49d7953db21db25
iaminnovator-07 <swadeepbansode2007@gmail.com>
2026-05-25 20:42:25 +0530

    Update heartbeat_monitoring_system.ino

--- 2020-early-experimentation/heartbeat-monitoring-system/src/heartbeat_monitoring_system.ino
+++ 2020-early-experimentation/heartbeat-monitoring-system/src/heartbeat_monitoring_system.ino
@@ -1,1 +1,31 @@
-// Placeholder
+int pulseSensor = A0;
+
+int led = 13;
+
+void setup() {
+
+  pinMode(led, OUTPUT);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  int pulseValue = analogRead(pulseSensor);
+
+  Serial.println(pulseValue);
+
+  if (pulseValue > 550) {
+
+    digitalWrite(led, HIGH);
+
+  } else {
+
+    digitalWrite(led, LOW);
+
+  }
+
+  delay(50);
+
+}
