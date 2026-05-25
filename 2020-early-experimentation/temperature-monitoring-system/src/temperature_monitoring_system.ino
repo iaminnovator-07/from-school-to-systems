58d00c7b258b64bd502514553356af24b3d237f7
iaminnovator-07 <swadeepbansode2007@gmail.com>
2026-05-25 20:51:46 +0530

    Update temperature_monitoring_system.ino

--- 2020-early-experimentation/temperature-monitoring-system/src/temperature_monitoring_system.ino
+++ 2020-early-experimentation/temperature-monitoring-system/src/temperature_monitoring_system.ino
@@ -1,1 +1,39 @@
-// Placeholder
+int tempSensor = A0;
+
+int led = 8;
+
+float temperature;
+
+void setup() {
+
+  pinMode(led, OUTPUT);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  int sensorValue = analogRead(tempSensor);
+
+  float voltage = sensorValue * (5.0 / 1023.0);
+
+  temperature = voltage * 100;
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" C");
+
+  if (temperature > 35) {
+
+    digitalWrite(led, HIGH);
+
+  } else {
+
+    digitalWrite(led, LOW);
+
+  }
+
+  delay(1000);
+
+}
