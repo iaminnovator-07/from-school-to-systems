a9fb22aacb1960eab221abd58fe19d748158b621
iaminnovator-07 <swadeepbansode2007@gmail.com>
2026-05-25 20:54:08 +0530

    Update ultrasonic_distance_measurement_system.ino

--- 2020-early-experimentation/ultrasonic-distance-measurement-system/src/ultrasonic_distance_measurement_system.ino
+++ 2020-early-experimentation/ultrasonic-distance-measurement-system/src/ultrasonic_distance_measurement_system.ino
@@ -1,1 +1,50 @@
-// Placeholder
+int trigPin = 9;
+int echoPin = 10;
+
+int led = 8;
+
+long duration;
+int distance;
+
+void setup() {
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  pinMode(led, OUTPUT);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+
+  distance = duration * 0.034 / 2;
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  if (distance < 10) {
+
+    digitalWrite(led, HIGH);
+
+  } else {
+
+    digitalWrite(led, LOW);
+
+  }
+
+  delay(200);
+
+}
