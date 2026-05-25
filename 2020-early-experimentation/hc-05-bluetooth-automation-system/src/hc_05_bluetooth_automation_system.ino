a7536794ae43607ddeaadc747e78fba5117bff6a
iaminnovator-07 <swadeepbansode2007@gmail.com>
2026-05-25 20:34:56 +0530

    Update hc_05_bluetooth_automation_system.ino

--- 2020-early-experimentation/hc-05-bluetooth-automation-system/src/hc_05_bluetooth_automation_system.ino
+++ 2020-early-experimentation/hc-05-bluetooth-automation-system/src/hc_05_bluetooth_automation_system.ino
@@ -1,1 +1,49 @@
-// Placeholder
+char data;
+
+int led1 = 8;
+int led2 = 9;
+
+void setup() {
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  if (Serial.available()) {
+
+    data = Serial.read();
+
+    Serial.println(data);
+
+    if (data == 'A') {
+
+      digitalWrite(led1, HIGH);
+
+    }
+
+    else if (data == 'a') {
+
+      digitalWrite(led1, LOW);
+
+    }
+
+    else if (data == 'B') {
+
+      digitalWrite(led2, HIGH);
+
+    }
+
+    else if (data == 'b') {
+
+      digitalWrite(led2, LOW);
+
+    }
+
+  }
+
+}
