5ee303ff08380f1fe87f30b7536aa64db4b19ddf
Swadeep Bansode <swadeepbansode2007@gmail.com>
2026-05-27 22:02:20 +0530

    Update aurora.ino

--- 2021-robotics-and-aurora/aurora/src/aurora.ino
+++ 2021-robotics-and-aurora/aurora/src/aurora.ino
@@ -1,1 +1,35 @@
-// Placeholder
+#include <SoftwareSerial.h>
+
+SoftwareSerial audioModule(10, 11);
+
+void setup() {
+
+  Serial.begin(9600);
+  audioModule.begin(9600);
+
+  Serial.println("Aurora Initialized");
+
+  delay(2000);
+
+  playAudio();
+
+}
+
+void loop() {
+
+}
+
+void playAudio() {
+
+  Serial.println("Playing Educational Audio");
+
+  audioModule.write(0x7E);
+  audioModule.write(0xFF);
+  audioModule.write(0x06);
+  audioModule.write(0x03);
+  audioModule.write(0x00);
+  audioModule.write(0x00);
+  audioModule.write(0x01);
+  audioModule.write(0xEF);
+
+}
