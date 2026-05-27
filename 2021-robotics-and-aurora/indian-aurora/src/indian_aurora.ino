96fcc0263f7af8059b3e6faed8801798447b6a91
Swadeep Bansode <swadeepbansode2007@gmail.com>
2026-05-27 22:14:36 +0530

    Update indian_aurora.ino

--- 2021-robotics-and-aurora/indian-aurora/src/indian_aurora.ino
+++ 2021-robotics-and-aurora/indian-aurora/src/indian_aurora.ino
@@ -1,1 +1,62 @@
-// Placeholder
+#include <SoftwareSerial.h>
+
+SoftwareSerial bluetooth(10, 11);
+
+char command;
+
+void setup() {
+
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+
+  Serial.println("Indian Aurora Initialized");
+
+}
+
+void loop() {
+
+  if (bluetooth.available()) {
+
+    command = bluetooth.read();
+
+    Serial.println(command);
+
+    if (command == 'A') {
+
+      playMaharashtraInfo();
+
+    }
+
+    else if (command == 'B') {
+
+      playGujaratInfo();
+
+    }
+
+    else if (command == 'C') {
+
+      playDelhiInfo();
+
+    }
+
+  }
+
+}
+
+void playMaharashtraInfo() {
+
+  Serial.println("Playing Maharashtra Information");
+
+}
+
+void playGujaratInfo() {
+
+  Serial.println("Playing Gujarat Information");
+
+}
+
+void playDelhiInfo() {
+
+  Serial.println("Playing Delhi Information");
+
+}
